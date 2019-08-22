e0b3ef96e1d57a3929e93eeb4fcac160ace0da57
Shantonav Sen <shantonav.sen@billinghouse.nl>
2019-08-22 13:57:32 +0200

    Date truncation

---
+++ src/test/java/test/com/solutions/DateTruncation.java
@@ -0,0 +1,31 @@
+package test.com.solutions;
+
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class DateTruncation {
+
+    @Test
+
+    public void testDateTruncationToOnlyMonthUsingSDF() throws ParseException {
+
+        Date today = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("MMyyyy");
+
+        today = sdf.parse(sdf.format(today));
+
+        System.out.println(today.toString());
+
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        System.out.println(sdf1.format(today));
+
+    }
+
+
+}
